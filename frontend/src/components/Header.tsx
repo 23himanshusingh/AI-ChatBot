854edd9759155d10f2549b1ed3c49d335c21e031
HIMANSHU SINGH <100193490+23himanshusingh@users.noreply.github.com>
2024-05-01 22:48:51 +0530

    first commit

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Logo from "./shared/Logo";
+import NavigationLink from "./shared/NavigationLink";
+
+const Header = () => {
+  return (
+    <AppBar
+      sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
+    >
+      <Toolbar sx={{ display: "flex" }}>
+        <Logo />
+        <div>
+              <NavigationLink
+                bg="#00fffc"
+                to="/chat"
+                text="Go To Chat"
+                textColor="black"
+              />
+              <NavigationLink
+                bg="#51538f"
+                textColor="white"
+                to="/"
+                text="logout"
+              />
+              <NavigationLink
+                bg="#00fffc"
+                to="/login"
+                text="Login"
+                textColor="black"
+              />
+              <NavigationLink
+                bg="#51538f"
+                textColor="white"
+                to="/signup"
+                text="Signup"
+              />
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div
+        style={{
+          width: "100%",
+          minHeight: "20vh",
+          maxHeight: "30vh",
+          marginTop: 60,
+        }}
+      >
+        <p style={{ fontSize: "30px", textAlign: "center", padding: "20px" }}>
+          Built by 21BPS1404 , 21BRS1667 , 21BRS1453
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
